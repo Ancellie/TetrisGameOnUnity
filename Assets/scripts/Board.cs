adf40e927d356fdc55c8b86867c285dcb90f491e
naaaaz <nazarii.rud@logicoapps.com>
2024-09-20 20:08:57 +0300

    Added spawning piece

--- Assets/scripts/Board.cs
+++ Assets/scripts/Board.cs
@@ -1,6 +1,47 @@
+using System;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.WSA;
+using Random = UnityEngine.Random;
 
 public class Board : MonoBehaviour
 {
+    public Tilemap tilemap { get; private set; }
+    public Piece activePiece { get; private set; }
     public TetrominoData[] tetrominoes;
+    public Vector3Int spawnposition;
+
+    private void Awake()
+    {
+        this.tilemap = GetComponentInChildren<Tilemap>();
+        this.activePiece = GetComponentInChildren<Piece>();
+        
+        for (int i = 0; i < this.tetrominoes.Length; i++)
+        {
+            this.tetrominoes[i].Initialize();
+        }
+    }
+
+    private void Start()
+    {
+        SpawnPiece();
+    }
+
+    public void SpawnPiece()
+    {
+        int random = Random.Range(0, this.tetrominoes.Length);
+        TetrominoData data = this.tetrominoes[random];
+        
+        this.activePiece.Initialize(this, this.spawnposition, data);
+        Set(this.activePiece);
+    }
+
+    public void Set(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, piece.data.tile);
+        }    
+    }
 }
